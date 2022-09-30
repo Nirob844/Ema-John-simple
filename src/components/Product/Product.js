56cc3bb205e86b2c1f03a419547bc7db61d28e4e
Nirob Hasan <nirob427417@gmail.com>
2022-10-01 00:09:35 +0600

    hi

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 
@@ -14,10 +16,12 @@ const Product = ({ handleAddToCart, product }) => {
                 <p><small>Rating: {ratings} starts</small></p>
             </div>
             <button onClick={() => handleAddToCart(product)} className='btn-cart'>
-                <p>Add to Card</p>
+                <p className='btn-text'>Add to Card</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div >
     );
 };
 
 export default Product;
+
