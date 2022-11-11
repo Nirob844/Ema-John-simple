b1d60dadcf00e99961f3baa4cd03bf2e7360da94
Nirob Hasan <nirob427417@gmail.com>
2022-11-11 22:03:01 +0600

    53

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, clearCart, children }) => {
     // console.log(cart)
     let total = 0;
     let shipping = 0;
@@ -22,7 +22,8 @@ const Cart = ({ cart }) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <p>Grand Total: ${grandTotal.toFixed(2)}</p>
-
+            <button onClick={clearCart}>Clear Cart</button>
+            {children}
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { addToDb, getStoreCart } from '../../utilities/fakedb';
+import { Link, useLoaderData } from 'react-router-dom';
+import { addToDb, deleteShoppingCart, getStoreCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -9,6 +9,10 @@ const Shop = () => {
     const products = useLoaderData();
     const [cart, setCart] = useState([]);
 
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
     useEffect(() => {
         const storedCart = getStoreCart();
         // console.log(storedCart);
@@ -51,7 +55,9 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart clearCart={clearCart} cart={cart}>
+                    <Link to={'/orders'}> <button>Review Orders</button> </Link>
+                </Cart>
             </div>
         </div>
     );
