471484b3c0f3c64fe33f33c803726f6b8e72e452
Nirob Hasan <nirob427417@gmail.com>
2023-01-20 19:15:05 +0600

    login-signup

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SignUp.css';
+
+const SignUp = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+    }
+
+    return (
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm">Confirm Password</label>
+                    <input type="password" name="confirm" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+            </form>
+            <p>Already Have an Account <Link to='/login'>Login</Link></p>
+            {/* <p className='text-error'>{error}</p> */}
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD8FxpyA2E0d4lfjQt6dQWiyQ69RXekP9M",
+    authDomain: "ema-john-72eb7.firebaseapp.com",
+    projectId: "ema-john-72eb7",
+    storageBucket: "ema-john-72eb7.appspot.com",
+    messagingSenderId: "832743619086",
+    appId: "1:832743619086:web:47e52a2acbe5ce40019a5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
